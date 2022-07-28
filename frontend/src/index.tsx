edc766602bba96280f9ff457554a391e033f0282
SSAFY <jisy2718@gmail.com>
2022-07-28 12:49:33 +0900

    webcam

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,17 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import App2 from './App2';
 import App3 from './App3';
-import Cam from './cam';
-import CamA from './cam_autoopen';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-
     <App3 />
-    // <Cam  />
-    // <CamA  />
-
 );
 
 // If you want to start measuring performance in your app, pass a function
