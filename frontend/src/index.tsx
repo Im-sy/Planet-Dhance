12206ee4acf191c697ee5baa3ff3e6b79af04431
eomsteve <mingyov0807@gmail.com>
2022-08-01 03:08:50 +0900

    react-thumnail generator

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App2 from './App3';
+import App2 from './App_video';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
