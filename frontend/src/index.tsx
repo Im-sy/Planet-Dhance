1542e1fffb223ccbb6fa76b8f6ff724aff5066c0
eomsteve <mingyov0807@gmail.com>
2022-08-02 10:32:08 +0900

    #S07P12D201-352 : update video_thumail_page async await

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import App2 from './App2';
+import App2 from './App_video';
 import ActionAreaCard from './Card';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  // <App2 />
-  <ActionAreaCard />
+  <App2 />
+  // <ActionAreaCard />
 );
 
 // If you want to start measuring performance in your app, pass a function
