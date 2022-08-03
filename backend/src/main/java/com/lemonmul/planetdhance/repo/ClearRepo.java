426ea00768364813c70294d6ece58059443db15b
Chosephine <backji5@gmail.com>
2022-08-04 01:50:40 +0900

    S07P12D201-290 feat: Add ClearApi, ClearRepo, ClearService & minor modify on MusicApi Comments

--- backend/src/main/java/com/lemonmul/planetdhance/repo/ClearRepo.java
+++ backend/src/main/java/com/lemonmul/planetdhance/repo/ClearRepo.java
@@ -1,8 +1,12 @@
 package com.lemonmul.planetdhance.repo;
 
 import com.lemonmul.planetdhance.entity.Clear;
+import com.lemonmul.planetdhance.entity.Video;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClearRepo extends JpaRepository<Clear, Long> {
-
+    /*
+    * mypage -> clear 테이블에서 user_id와 일치하는 거 찾기 -> 해당하는 music 반환
+    * */
+//    List<Music> findByUserId
 }
