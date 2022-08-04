23f498a9407efce7115435ef963ce26744a0aefc
1217jdk <1217jdk@naver.com>
2022-08-05 03:37:57 +0900

    fix: fix jwt error

--- backend/src/main/java/com/lemonmul/planetdhance/security/jwt/JwtToken.java
+++ backend/src/main/java/com/lemonmul/planetdhance/security/jwt/JwtToken.java
@@ -1,70 +1,18 @@
 package com.lemonmul.planetdhance.security.jwt;
 
-import com.lemonmul.planetdhance.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.ElementCollection;
 import javax.persistence.FetchType;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Getter
 @AllArgsConstructor
-public class JwtToken implements UserDetails {
+public class JwtToken {
     private String email;
     private String nickname;
     private String nationName;
     @ElementCollection(fetch = FetchType.EAGER)
     private List<String> roles;
-
-    public JwtToken(User user){
-        this.email = user.getEmail();
-        this.nickname = user.getNickname();
-        this.nationName = user.getNation().getName();
-        this.roles = Collections.singletonList(user.getRole().getKey());
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return this.roles.stream()
-                .map(SimpleGrantedAuthority::new)
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public String getPassword() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return null;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
 }
