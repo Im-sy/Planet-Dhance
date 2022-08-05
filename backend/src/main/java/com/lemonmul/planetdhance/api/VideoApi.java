d35ba714bc86a7871230125b167c463453d4ea3b
zaq1290 <zaq1290@naver.com>
2022-08-05 17:45:09 +0900

    feat: Add Music Api - GET /music/list/{music_id}, Modify Video Api - GET /video/list/{music_id}/latest/{page}, GET /video/list/{music_id}/hitlike/{page}

--- backend/src/main/java/com/lemonmul/planetdhance/api/VideoApi.java
+++ backend/src/main/java/com/lemonmul/planetdhance/api/VideoApi.java
@@ -1,8 +1,10 @@
 package com.lemonmul.planetdhance.api;
 
 import com.lemonmul.planetdhance.dto.VideoDto;
+import com.lemonmul.planetdhance.entity.Music;
 import com.lemonmul.planetdhance.entity.video.Video;
 import com.lemonmul.planetdhance.entity.video.VideoScope;
+import com.lemonmul.planetdhance.service.MusicService;
 import com.lemonmul.planetdhance.service.VideoService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -20,30 +22,33 @@ import org.springframework.web.bind.annotation.RestController;
 public class VideoApi {
 
     private final VideoService videoService;
+    private final MusicService musicService;
 
-    private static final int size=18;
+    private static final int size=5;
 
     /**
      * 영상 리스트 - 최신
      *
-     * 요청 파라미터 예시: /video/list/latest/{page번호}
+     * 요청 파라미터 예시: /video/list/{곡 아이디}/latest/{page번호}
      * size는 기본값 18
      */
-    @GetMapping("/list/latest/{page}")
-    public Slice<VideoDto> newestList(@PathVariable int page){
-        Slice<Video> videoList = videoService.findNewestVideoList(page,size, VideoScope.PUBLIC);
+    @GetMapping("/list/{music_id}/latest/{page}")
+    public Slice<VideoDto> newestList(@PathVariable Long music_id,@PathVariable int page){
+        Music music=musicService.getMusicInfo(music_id).get();
+        Slice<Video> videoList = videoService.findNewestVideoList(page,size,music, VideoScope.PUBLIC);
         return videoList.map(VideoDto::new);
     }
 
     /**
      * 영상 리스트 - 조회수&좋아요
      *
-     * 요청 파라미터 예시: /video/list/hitlike/{page번호}
+     * 요청 파라미터 예시: /video/list/{곡 아이디}/hitlike/{page번호}
      * size는 기본값 18
      */
-    @GetMapping("/list/hitlike/{page}")
-    public Slice<VideoDto> hitlikeList(@PathVariable int page){
-        Slice<Video> videoList = videoService.findHitLikeVideoList(page,size, VideoScope.PUBLIC);
+    @GetMapping("/list/{music_id}/hitlike/{page}")
+    public Slice<VideoDto> hitlikeList(@PathVariable Long music_id,@PathVariable int page){
+        Music music=musicService.getMusicInfo(music_id).get();
+        Slice<Video> videoList = videoService.findHitLikeVideoList(page,size,music, VideoScope.PUBLIC);
         return videoList.map(VideoDto::new);
     }
 
