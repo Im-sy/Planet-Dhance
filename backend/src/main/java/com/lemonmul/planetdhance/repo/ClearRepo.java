513ba7722d1e8e7468175dbf7355abb64e956ef6
Chosephine <backji5@gmail.com>
2022-08-06 19:22:54 +0900

    #S07P12D201-304 Style: delete unnecessary comment on ClearRepo

--- backend/src/main/java/com/lemonmul/planetdhance/repo/ClearRepo.java
+++ backend/src/main/java/com/lemonmul/planetdhance/repo/ClearRepo.java
@@ -11,6 +11,5 @@ public interface ClearRepo extends JpaRepository<Clear, Long> {
      * mypage -> clear 테이블에서 user_id와 일치하는 거 찾기 -> 해당하는 music 반환
      * */
     List<Clear> findClearsByUserOrderByIdDesc(User user);
-//    List<Clear> findClearsByUserOrderByMusicRelDate();
 
 }
