42f44a0e81425b0bcade297e1f3bd648ff802a53
zaq1290 <zaq1290@naver.com>
2022-08-15 22:56:18 +0900

    test: add nation and modify videos

--- backend/src/test/java/com/lemonmul/planetdhance/service/VideoServiceTest.java
+++ backend/src/test/java/com/lemonmul/planetdhance/service/VideoServiceTest.java
@@ -141,6 +141,9 @@ class VideoServiceTest {
         nation=Nation.createNation("\uD83C\uDDE6\uD83C\uDDFA","Australia","/resource/nation/img/australia.png",-0.3,-4.5,-0.7);
         em.persist(nation);
         em.persist(Tag.createTag(nation.getName(), TagType.NATION,nation.getImgUrl()));
+        nation=Nation.createNation("\uD83E\uDE90","Dhance","/resource/nation/img/dhance.png",-3,-3,-2.5);
+        em.persist(nation);
+        em.persist(Tag.createTag(nation.getName(), TagType.NATION,nation.getImgUrl()));
     }
 
     private void createUserTable(){
@@ -152,7 +155,6 @@ class VideoServiceTest {
         em.persist(Tag.createTag(user2.getNickname(),TagType.NICKNAME,user2.getImgUrl()));
     }
 
-    //TODO 곡 모델 경로
     private void createArtistAndMusicTable(){
         Artist artist=Artist.createArtist("NAYEON","/resource/artist/img/nayeon.jpg");
         artist.setOrderWeight(5L);
@@ -165,28 +167,28 @@ class VideoServiceTest {
         artist=Artist.createArtist("BTS","/resource/artist/img/bts.JPG");
         artist.setOrderWeight(20L);
         em.persist(artist);
-        music=Music.createMusic("Permission to Dance",artist,"/resource/artist/img/PtoD_img.jpg","model url1","/resource/artist/video/PtoD_guide.mp4","https://youtu.be/CuklIb9d3fI", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("Permission to Dance",artist,"/resource/artist/img/PtoD_img.jpg","model url1","/resource/artist/video/PtoD_guide.mp4","https://youtu.be/CuklIb9d3fI", LocalDateTime.of(2021,7,9,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
 
         artist=Artist.createArtist("CHUNG HA","/resource/artist/img/chungha.jpg");
         em.persist(artist);
-        music=Music.createMusic("Sparkling",artist,"/resource/artist/img/sparkling_img.jpg","model url1","/resource/artist/video/sparkling_guide.mp4","https://youtu.be/lDV5cM9YE4g", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("Sparkling",artist,"/resource/artist/img/sparkling_img.jpg","model url1","/resource/artist/video/sparkling_guide.mp4","https://youtu.be/lDV5cM9YE4g", LocalDateTime.of(2022,7,11,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
 
         artist=Artist.createArtist("TWICE","/resource/artist/img/twice.jpg");
         em.persist(artist);
-        music=Music.createMusic("TT",artist,"/resource/artist/img/TT_img.jpg","model url1","/resource/artist/video/TT_guide.mp4","https://youtu.be/ePpPVE-GGJw", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("TT",artist,"/resource/artist/img/TT_img.jpg","model url1","/resource/artist/video/TT_guide.mp4","https://youtu.be/ePpPVE-GGJw", LocalDateTime.of(2016,10,24,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
 
         artist=Artist.createArtist("Girls' Generation","/resource/artist/img/snsd.jpg");
         em.persist(artist);
-        music=Music.createMusic("FOREVER 1",artist,"/resource/artist/img/Forever1_img.jpg","model url1","/resource/artist/video/Forever1_guide.mp4","https://youtu.be/Qpf26PtBXgo", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("FOREVER 1",artist,"/resource/artist/img/Forever1_img.jpg","model url1","/resource/artist/video/Forever1_guide.mp4","https://youtu.be/Qpf26PtBXgo", LocalDateTime.of(2022,8,5,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
@@ -201,7 +203,7 @@ class VideoServiceTest {
         artist=Artist.createArtist("IVE","/resource/artist/img/ive.jpg");
         artist.setOrderWeight(2L);
         em.persist(artist);
-        music=Music.createMusic("LOVE DIVE",artist,"/resource/artist/img/lovedive_img.jpg","model url1","/resource/artist/video/LoveDive_guide.mp4","https://youtu.be/Y8JFxS1HlDo", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("LOVE DIVE",artist,"/resource/artist/img/lovedive_img.jpg","model url1","/resource/artist/video/LoveDive_guide.mp4","https://youtu.be/Y8JFxS1HlDo", LocalDateTime.of(2020,4,5,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
@@ -209,14 +211,14 @@ class VideoServiceTest {
         artist=Artist.createArtist("IU","/resource/artist/img/iu.jpg");
         artist.setOrderWeight(4L);
         em.persist(artist);
-        music=Music.createMusic("my dream patissiere",artist,"/resource/artist/img/Patissiere_img.png","model url1","/resource/artist/video/Patissiere_guide.mp4","https://youtu.be/bgQIzPnPI88", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("my dream patissiere",artist,"/resource/artist/img/Patissiere_img.png","model url1","/resource/artist/video/Patissiere_guide.mp4","https://youtu.be/bgQIzPnPI88", LocalDateTime.of(2010,11,13,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
 
         artist=Artist.createArtist("SEVENTEEN","/resource/artist/img/seventeen.jpeg");
         em.persist(artist);
-        music=Music.createMusic("HOT",artist,"/resource/artist/img/hot_img.jpg","model url1","/resource/artist/video/hot_guide.mp4","https://youtu.be/gRnuFC4Ualw", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("HOT",artist,"/resource/artist/img/hot_img.jpg","model url1","/resource/artist/video/hot_guide.mp4","https://youtu.be/gRnuFC4Ualw", LocalDateTime.of(2022,5,27,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
@@ -224,14 +226,14 @@ class VideoServiceTest {
         artist=Artist.createArtist("SUPER JUNIOR","/resource/artist/img/superjunior.jpg");
         artist.setOrderWeight(3L);
         em.persist(artist);
-        music=Music.createMusic("Sorry, Sorry",artist,"/resource/artist/img/SorrySorry_img.jpg","model url1","/resource/artist/video/SorrySorry_guide.mp4","https://youtu.be/x6QA3m58DQw", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("Sorry, Sorry",artist,"/resource/artist/img/SorrySorry_img.jpg","model url1","/resource/artist/video/SorrySorry_guide.mp4","https://youtu.be/x6QA3m58DQw", LocalDateTime.of(2022,3,12,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
 
         artist=Artist.createArtist("(G)I-DLE","/resource/artist/img/gidle.jpg");
         em.persist(artist);
-        music=Music.createMusic("TOMBOY",artist,"/resource/artist/img/Tomboy_img.jpg","model url1","/resource/artist/video/Tomboy_guide.mp4","https://youtu.be/Jh4QFaPmdss", LocalDateTime.of(2022,6,24,0,0));
+        music=Music.createMusic("TOMBOY",artist,"/resource/artist/img/Tomboy_img.jpg","model url1","/resource/artist/video/Tomboy_guide.mp4","https://youtu.be/Jh4QFaPmdss", LocalDateTime.of(2022,3,14,0,0));
         em.persist(music);
         em.persist(Tag.createTag(music.getArtist().getName(),TagType.ARTIST,music.getArtist().getImgUrl()));
         em.persist(Tag.createTag(music.getTitle(),TagType.TITLE, music.getImgUrl()));
@@ -277,14 +279,19 @@ class VideoServiceTest {
 ////        em.persist(music3);
         createArtistAndMusicTable();
 
+        User user1=userRepo.findByNickname("user1").get();
+        User user2=userRepo.findByNickname("user2").get();
+        Music music1=musicRepo.findTestByTitle("Permission to Dance");
+        Music music2=musicRepo.findTestByTitle("POP!");
+
         //video, like, tag
         List<Video> videos=new ArrayList<>();
         for(int i=0;i<5;i++){
-            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm", "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png", VideoScope.PUBLIC, userRepo.findByNickname("user1").get(), musicRepo.findById(32l).get());
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user1").get().getNickname(),TagType.NICKNAME));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user1").get().getNation().getName(),TagType.NATION));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(32l).get().getArtist().getName(),TagType.ARTIST));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(32l).get().getTitle(),TagType.TITLE));
+            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm", "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png", VideoScope.PUBLIC, user1,music1);
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user1.getNickname(),TagType.NICKNAME));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user1.getNation().getName(),TagType.NATION));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music1.getArtist().getName(),TagType.ARTIST));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music1.getTitle(),TagType.TITLE));
             for(int j=0;j<i;j++){
                 video.addHit();
             }
@@ -293,11 +300,11 @@ class VideoServiceTest {
             em.persist(video);
         }
         for(int i=5;i<10;i++){
-            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm",  "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png", VideoScope.PUBLIC,userRepo.findByNickname("user1").get(), musicRepo.findById(28l).get());
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user1").get().getNickname(),TagType.NICKNAME));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user1").get().getNation().getName(),TagType.NATION));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(28l).get().getArtist().getName(),TagType.ARTIST));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(28l).get().getTitle(),TagType.TITLE));
+            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm",  "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png", VideoScope.PUBLIC,user1,music2);
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user1.getNickname(),TagType.NICKNAME));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user1.getNation().getName(),TagType.NATION));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music2.getArtist().getName(),TagType.ARTIST));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music2.getTitle(),TagType.TITLE));
             for(int j=4;j<i;j++){
                 video.addHit();
             }
@@ -305,8 +312,8 @@ class VideoServiceTest {
             video.addLikeWeight();
             videos.add(video);
             em.persist(video);
-            em.persist(Like.createLike(video,userRepo.findByNickname("user1").get()));
-            em.persist(Like.createLike(video,userRepo.findByNickname("user2").get()));
+            em.persist(Like.createLike(video,user1));
+            em.persist(Like.createLike(video,user2));
         }
 //        Video video1 = Video.createVideo("video url!!", VideoScope.PRIVATE, "thumbnail url!!", user2, music1);
 //        VideoTag.createVideoTag(video1,tagRepo.findByNameAndType(user2.getNickname(),TagType.NICKNAME));
@@ -317,11 +324,11 @@ class VideoServiceTest {
 //        em.persist(video1);
 //        em.persist(Like.createLike(video1,user2));
         for(int i=10;i<15;i++){
-            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm",  "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png",VideoScope.PUBLIC,  userRepo.findByNickname("user2").get(), musicRepo.findById(28l).get());
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user2").get().getNickname(),TagType.NICKNAME));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user2").get().getNation().getName(),TagType.NATION));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(28l).get().getArtist().getName(),TagType.ARTIST));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(28l).get().getTitle(),TagType.TITLE));
+            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm",  "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png",VideoScope.PUBLIC,user2, music2);
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user2.getNickname(),TagType.NICKNAME));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user2.getNation().getName(),TagType.NATION));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music2.getArtist().getName(),TagType.ARTIST));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music2.getTitle(),TagType.TITLE));
             for(int j=7;j<i;j++){
                 video.addHit();
             }
@@ -329,11 +336,11 @@ class VideoServiceTest {
             em.persist(video);
         }
         for(int i=16;i<20;i++){
-            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm",  "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png", VideoScope.PUBLIC,  userRepo.findByNickname("user2").get(), musicRepo.findById(28l).get());
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user2").get().getNickname(),TagType.NICKNAME));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(userRepo.findByNickname("user2").get().getNation().getName(),TagType.NATION));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(28l).get().getArtist().getName(),TagType.ARTIST));
-            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(musicRepo.findById(28l).get().getTitle(),TagType.TITLE));
+            Video video = Video.createVideo("/temp/c2d6f99a-8e0c-4413-a8ab-6e809b6b7d1b.webm",  "/temp/9e7de94f-a832-4af3-9125-fbdd90277b18.png", VideoScope.PUBLIC, user2,music2);
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user2.getNickname(),TagType.NICKNAME));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(user2.getNation().getName(),TagType.NATION));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music2.getArtist().getName(),TagType.ARTIST));
+            VideoTag.createVideoTag(video,tagRepo.findTestByNameAndType(music2.getTitle(),TagType.TITLE));
             for(int j=15;j<i;j++){
                 video.addHit();
             }
@@ -344,14 +351,14 @@ class VideoServiceTest {
         }
 
         //clear
-        em.persist(Clear.createClear(musicRepo.findById(32l).get(),userRepo.findByNickname("user1").get()));
+        em.persist(Clear.createClear(music1,user1));
 //        em.persist(Clear.createClear(music1,user2));
-        em.persist(Clear.createClear(musicRepo.findById(28l).get(),userRepo.findByNickname("user1").get()));
-        em.persist(Clear.createClear(musicRepo.findById(28l).get(),userRepo.findByNickname("user2").get()));
+        em.persist(Clear.createClear(music2,user1));
+        em.persist(Clear.createClear(music2,user2));
 
         //follow
-        em.persist(Follow.createFollow(userRepo.findByNickname("user1").get(),userRepo.findByNickname("user2").get()));
-        em.persist(Follow.createFollow(userRepo.findByNickname("user2").get(),userRepo.findByNickname("user1").get()));
+        em.persist(Follow.createFollow(user1,user2));
+        em.persist(Follow.createFollow(user2,user1));
     }
 
 }
